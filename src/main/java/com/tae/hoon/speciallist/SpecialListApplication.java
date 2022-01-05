ce71166b32bb3dbfd3993e65317f9eb670a23093
choitaehoon1998 <51841238+choitaehoon1998@users.noreply.github.com>
2022-01-05 19:14:14 +0900

    AA
    AASD

---
+++ src/main/java/com/tae/hoon/speciallist/SpecialListApplication.java
@@ -0,0 +1,13 @@
+package com.tae.hoon.speciallist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpecialListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tae/hoon/speciallist/SpecialListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tae.hoon.speciallist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
